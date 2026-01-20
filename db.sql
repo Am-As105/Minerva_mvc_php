4842e59291350049ab0ed948a621518f9518ef07
amine <aminear220@gmail.com>
2026-01-20 17:02:47 +0100

    databases tables sql

---
+++ db.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('teacher', 'student') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    teacher_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (teacher_id) REFERENCES users(id)
+);
+CREATE TABLE class_students (
+    class_id INT NOT NULL,
+    student_id INT NOT NULL,
+    PRIMARY KEY (class_id, student_id),
+    FOREIGN KEY (class_id) REFERENCES classes(id),
+    FOREIGN KEY (student_id) REFERENCES users(id)
+);
+
+CREATE TABLE works (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    class_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    file_path VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (class_id) REFERENCES classes(id)
+);
+
+CREATE TABLE work_assignments (
+    work_id INT NOT NULL,
+    student_id INT NOT NULL,
+    PRIMARY KEY (work_id, student_id),
+    FOREIGN KEY (work_id) REFERENCES works(id),
+    FOREIGN KEY (student_id) REFERENCES users(id)
+);
